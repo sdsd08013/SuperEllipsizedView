53f8f894f112dc38c3ca4c41354256326ba23cd9
sdsd08013 <sd08013+git@gmail.com>
2023-03-26 19:34:53 +0900

    minor fix

--- app/src/main/java/lib/sky/ellipseexample/MainActivity.kt
+++ app/src/main/java/lib/sky/ellipseexample/MainActivity.kt
@@ -2,7 +2,6 @@ package lib.sky.ellipseexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import lib.sky.superellipsizedview.views.SuperEllipsizedImage
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
